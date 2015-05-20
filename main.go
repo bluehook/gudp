627d0980f482d674a708b18dfb6db0e76677e0c9
bluehook <bluehook@126.com>
2015-05-20 14:59:31 +0800

    测试MAP和LIST性能，MAP插入和遍历速度为LIST的2倍，但是查找速度MAP比LIST高几个数量级！

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"./network"
+	"container/list"
 	"fmt"
+	"strconv"
 	"time"
 )
 
@@ -70,10 +72,90 @@ func test_networkudp() {
 	client.Close()
 }
 
+func testingList() (int64, int64, int64) {
+
+	cur := time.Now().UnixNano()
+	testList := list.New()
+	for i := 0; i < 1e5; i++ {
+		testList.PushBack("id" + strconv.Itoa(i))
+	}
+	use1 := (time.Now().UnixNano() - cur) / 1e6
+	cur = time.Now().UnixNano()
+	nameNum := 0
+	for e := testList.Front(); e != nil; e = e.Next() {
+		nameNum += len(e.Value.(string))
+	}
+	use2 := (time.Now().UnixNano() - cur) / 1e6
+	cur = time.Now().UnixNano()
+	for i := 0; i < 10; i++ {
+		for e := testList.Front(); e != nil; e = e.Next() {
+			if e.Value == "id100" {
+				e.Value = "id3333"
+			}
+		}
+	}
+	use3 := (time.Now().UnixNano() - cur) / 1e6
+	return use1, use2, use3
+}
+
+func testingMap() (int64, int64, int64) {
+
+	testMap := make(map[string]string)
+	cur := time.Now().UnixNano()
+
+	for i := 0; i < 1e5; i++ {
+		idname := "id" + strconv.Itoa(i)
+		testMap[idname] = idname
+	}
+	use1 := (time.Now().UnixNano() - cur) / 1e6
+	cur = time.Now().UnixNano()
+	nameNum := 0
+
+	for _, name := range testMap {
+		nameNum += len(name)
+	}
+	use2 := (time.Now().UnixNano() - cur) / 1e6
+	cur = time.Now().UnixNano()
+	for i := 0; i < 1e5; i++ {
+		testMap["id43223"] = "id3333"
+	}
+	use3 := (time.Now().UnixNano() - cur) / 1e6
+	return use1, use2, use3
+}
+
+func testingListAndMap() {
+
+	var count1, count2, count3 int64
+	for i := 0; i < 50; i++ {
+		use1, use2, use3 := testingMap()
+		count1 += use1
+		count2 += use2
+		count3 += use3
+	}
+	count1 /= 50
+	count2 /= 50
+	count3 /= 50
+	fmt.Println("MAP平均插入100000个元素,遍历,查询分别用时（毫秒）：", count1, count2, count3)
+
+	count1, count2, count3 = 0, 0, 0
+	for i := 0; i < 50; i++ {
+		use1, use2, use3 := testingList()
+		count1 += use1
+		count2 += use2
+		count3 += use3
+	}
+	count1 /= 50
+	count2 /= 50
+	count3 /= 50
+	fmt.Println("List平均插入100000个元素,遍历,查询分别用时（毫秒）：", count1, count2, count3)
+}
+
 func main() {
+
 	fmt.Println("GUDP")
+	//testingListAndMap()
 	test_networkudp()
 	for {
-		time.Sleep(1e9)
+		<-time.After(1e9)
 	}
 }
